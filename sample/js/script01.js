4e2352d3a36632c0f5a6058e38357b9adf523746
GF13-017 <68629581+GF13-017@users.noreply.github.com>
2020-10-23 16:04:45 +0900

    ssss

--- sample/js/script01.js
+++ sample/js/script01.js
@@ -1,6 +1,50 @@
 $(function(){
 
+    $(function(){
+ 
+        let txt1 = "おはようございます！";
+        let txt2 = "おげんきですか？";
+        let num1 = 20;
+        let num2 = 32;
+        function seyHello(){
+            $("#content").text("こんにちは！");
+        }
+        function saysomehing(txt){
 
+        }
+        function samTwo(arg1,arg2){
+            $("#content").text(arg1,arg2);
+
+
+        }
+        　　
+
+        $("#button01").on("click",function(){
+          seyHello();
+        })
+        
+        $("#button02").on("click",function(){
+          sayasomething(txt1);
+        })
+        
+        $("#button03").on("click",function(){
+          samTwo(txt1 , txt2);
+        })
+        
+        $("#button04").on("click",function(){
+          samTwo(num1,num2)
+        })
+        
+        // + - * /
+        
+        $("#button05").on("click",function(){
+            samTwo(txt1 , txt2);
+            samTwo(num1,num2)
+        })
+        
+        
+       })
+       
 
 
 })
--- sample/js/script02.js
+++ sample/js/script02.js
@@ -1,5 +1,16 @@
 $(function(){
+　　
+let hako = 0;
 
+function checkBox(){
+    if(hako > 5){
+        s("#content").text(hako + "個入っています。箱はいっぱいです。からにします。")；
+        hako = 0;
+    }else
+        s("//#content").text(hako + )
+
+
+}
 
 
 })
